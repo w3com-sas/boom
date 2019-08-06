27c5a957dd5ee7d9c00e298598023af4b33fb728
Sébastien DREAN <sdrean@w3com.fr>
2019-08-06 14:50:26 +0200

    - Added new method to use hanaConfigManager

--- Service/HanaConfigManager.php
+++ Service/HanaConfigManager.php
@@ -2,8 +2,8 @@
 
 namespace W3com\BoomBundle\Service;
 
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
-use W3com\BoomBundle\Service\BoomManager;
 
 /**
  * Class HanaConfigManager.
@@ -37,12 +37,11 @@ class HanaConfigManager
     /**
      * @return array
      *
-     * @throws \Psr\Cache\InvalidArgumentException
+     * @throws InvalidArgumentException
      * @throws \Exception
      */
     public function getConfig(): array
     {
-
         $cachedConfig = $this->cache->getItem($this::STORAGE_KEY);
         if (!$cachedConfig->isHit()) {
             $configAr = $this->boom->getRepository('Config')->findAll();
@@ -60,5 +59,60 @@ class HanaConfigManager
         }
 
         return $config;
+    }
+
+    /**
+     * @param $configType
+     * @return mixed
+     * @throws InvalidArgumentException
+     */
+    public function getConfigTypeList($configType)
+    {
+        $config = $this->getConfig();
+        if(!array_key_exists($configType,$config)){
+            throw new \Exception('Liste '.$configType.' inconnue');
+        }
+        return $config[$configType];
+    }
+
+    /**
+     * @param $configType
+     * @param $value
+     * @return int|string
+     * @throws InvalidArgumentException
+     */
+    public function getKey($configType, $value)
+    {
+        $list = $this->getConfigTypeList($configType);
+        if(!is_array($list) || count($list) == 0){
+            throw new \Exception('Liste '.$configType.' inconnue ou vide');
+        }
+        foreach($list as $configKey=>$configValue){
+            if($configValue == $value){
+                return $configKey;
+            }
+        }
+        throw new \Exception('La valeur demandée n\'a pas été trouvé dans la liste '.$configType);
+    }
+
+    /**
+     * @param $configType
+     * @param $key
+     * @return mixed
+     * @throws InvalidArgumentException
+     * @throws \Exception
+     */
+    public function getValue($configType, $key)
+    {
+        $list = $this->getConfigTypeList($configType);
+        if(!is_array($list) || count($list) == 0){
+            throw new \Exception('Liste '.$configType.' inconnue ou vide');
+        }
+        foreach($list as $configKey=>$configValue){
+            if($configKey == $key){
+                return $configValue;
+            }
+        }
+        throw new \Exception('La clé demandée n\'a pas été trouvé dans la liste '.$configType);
     }
 }
