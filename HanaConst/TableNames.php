c07cf91c0fce983d03dd4d2dce918fa349c21b5b
Sébastien DREAN <sdrean@w3com.fr>
2019-12-11 15:14:31 +0100

    - Adding 1 new tables name in HanaConst

--- HanaConst/TableNames.php
+++ HanaConst/TableNames.php
@@ -29,4 +29,5 @@ class TableNames
     const SALESOPPORTUNITIES     = 'OOPR';
     const INCOMINGPAYMENTS       = 'ORCT';
     const VENDORPAYMENTS         = 'OVPM';
+    const BANKPAGES              = 'OACT';
 }
