b9646257aa552eae1ba82b19d6717e42637ef5e7
corentin-gillet <cgillet@w3com.fr>
2019-12-23 17:19:53 +0100

    No login when badRequestCode === 400

--- RestClient/SLRestClient.php
+++ RestClient/SLRestClient.php
@@ -69,9 +69,6 @@ class SLRestClient implements RestClientInterface
             } catch (ClientException $e) {
                 if (401 == $e->getCode()) {
                     $this->login();
-                } elseif(400 == $e->getCode()){
-                    $this->manager->logger->info($e->getCode().' - '.$e->getMessage().' : '.$uri);
-                    $this->login();
                 } else {
                     $stop = $this->manager->stopwatch->stop('SL-get');
                     $response = $e->getResponse()->getBody()->getContents();
