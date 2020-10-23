bf4587d455352309d8e8c37afec025ed9c068806
Maxime.Saulière <msauliere@w3com.fr>
2020-10-23 14:30:47 +0200

    Display checkbox in form when choices annotation in HanaEntity is Y or N (BoomGuesser + SapCheckboxType)

--- Form/BoomTypeGuesser.php
+++ Form/BoomTypeGuesser.php
@@ -2,8 +2,9 @@
 
 namespace W3com\BoomBundle\Form;
 
-
 use Doctrine\Common\Annotations\AnnotationReader;
+use ReflectionException;
+use ReflectionProperty;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
@@ -13,22 +14,23 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormTypeGuesserInterface;
 use Symfony\Component\Form\Guess\Guess;
 use Symfony\Component\Form\Guess\TypeGuess;
+use W3com\BoomBundle\Form\Type\SapCheckboxType;
 use W3com\BoomBundle\Form\Type\SapDateType;
 use W3com\BoomBundle\Form\Type\SapTimeType;
 
 class BoomTypeGuesser implements FormTypeGuesserInterface
 {
-
-
     /**
      * Returns a field guess for a property name of a class.
      *
      * @param string $class The fully qualified class name
      * @param string $property The name of the property to guess for
      *
-     * @return TypeGuess
+     * @return void|null|TypeGuess
+     *
+     * @throws ReflectionException
      */
-    public function guessType($class, $property)
+    public function guessType(string $class, string $property)
     {
         $annotations = $this->readPhpDocAnnotations($class, $property);
 
@@ -41,46 +43,104 @@ class BoomTypeGuesser implements FormTypeGuesserInterface
             case 'string':
                 // there is a high confidence that the type is text when
                 // @var string is used
-                return new TypeGuess(TextType::class, ['label' => $annotations['label']]
-                    , Guess::HIGH_CONFIDENCE);
-
+                return new TypeGuess(
+                    TextType::class,
+                    [
+                        'label' => $annotations['label']
+                    ]
+                    , Guess::HIGH_CONFIDENCE
+                );
             case 'choice':
                 $choicesElement = explode('#' ,$annotations['choices']);
                 $choices = [];
                 if(count($choicesElement) > 0){
-                    foreach($choicesElement as $element){
+                    foreach($choicesElement as $element) {
                         $tmp = explode('|',$element);
-                        if(count($tmp) == 2){
+
+                        // Condition to trigger the SapCheckboxType class
+                        if ($tmp[0] === 'Oui' && $tmp[1] === 'Y') {
+                            $choices[$tmp[0]] = $tmp[1];
+                        } elseif ($tmp[0] === 'Non' && $tmp[1] === 'N') {
+                            $choices[$tmp[0]] = $tmp[1];
+                        }
+
+                        if (count($tmp) == 2) {
                             $choices[$tmp[0]] = $tmp[1];
                         } else {
                             $choices[$element] = $element;
                         }
-
+                    }
+                    if (array_key_exists('Oui', $choices) && array_key_exists('Non', $choices)) {
+                        return new TypeGuess(SapCheckboxType::class,
+                            [
+                                'label' => $annotations['label'],
+                            ],
+                            Guess::HIGH_CONFIDENCE
+                        );
                     }
                 }
-                return new TypeGuess(ChoiceType::class, ['label' => $annotations['label'], 'choices' => $choices], Guess::HIGH_CONFIDENCE);
-
+                return new TypeGuess(ChoiceType::class,
+                    [
+                        'label' => $annotations['label'],
+                        'choices' => $choices,
+                    ],
+                    Guess::HIGH_CONFIDENCE
+                );
             case 'int':
             case 'integer':
                 // integers can also be the id of an entity or a checkbox (0 or 1)
-                return new TypeGuess(IntegerType::class, ['label' => $annotations['label']], Guess::MEDIUM_CONFIDENCE);
+                return new TypeGuess(IntegerType::class,
+                    [
+                        'label' => $annotations['label'],
+                    ],
+                    Guess::MEDIUM_CONFIDENCE
+                );
             case 'date':
-                return new TypeGuess(SapDateType::class, ['label' => $annotations['label']], Guess::MEDIUM_CONFIDENCE);
+                return new TypeGuess(SapDateType::class,
+                    [
+                        'label' => $annotations['label'],
+                    ],
+                    Guess::MEDIUM_CONFIDENCE
+                );
             case 'time':
-                return new TypeGuess(SapTimeType::class, ['label' => $annotations['label']], Guess::MEDIUM_CONFIDENCE);
+                return new TypeGuess(SapTimeType::class,
+                    [
+                        'label' => $annotations['label'],
+                    ],
+                    Guess::MEDIUM_CONFIDENCE
+                );
             case 'float':
             case 'double':
             case 'real':
-                return new TypeGuess(NumberType::class, ['label' => $annotations['label']], Guess::MEDIUM_CONFIDENCE);
-
+                return new TypeGuess(NumberType::class,
+                    [
+                        'label' => $annotations['label'],
+                    ],
+                    Guess::MEDIUM_CONFIDENCE
+                );
             case 'boolean':
             case 'bool':
-                return new TypeGuess(CheckboxType::class, ['label' => $annotations['label']], Guess::HIGH_CONFIDENCE);
+                return new TypeGuess(CheckboxType::class,
+                    [
+                        'label' => $annotations['label,']
+                    ],
+                    Guess::HIGH_CONFIDENCE
+                );
             case 'hidden':
-                return new TypeGuess(HiddenType::class, ['label' => $annotations['label']], Guess::MEDIUM_CONFIDENCE);
+                return new TypeGuess(HiddenType::class,
+                    [
+                        'label' => $annotations['label'],
+                    ],
+                    Guess::MEDIUM_CONFIDENCE
+                );
             default:
                 // there is a very low confidence that this one is correct
-                return new TypeGuess(HiddenType::class, ['label' => $annotations['label']], Guess::LOW_CONFIDENCE);
+                return new TypeGuess(HiddenType::class,
+                    [
+                        'label' => $annotations['labe,l']
+                    ],
+                    Guess::LOW_CONFIDENCE
+                );
         }
     }
 
@@ -92,7 +152,7 @@ class BoomTypeGuesser implements FormTypeGuesserInterface
      *
      * @return void A guess for the field's required setting
      */
-    public function guessRequired($class, $property)
+    public function guessRequired(string $class, string $property)
     {
         return null;
     }
@@ -105,7 +165,7 @@ class BoomTypeGuesser implements FormTypeGuesserInterface
      *
      * @return Guess\ValueGuess|null A guess for the field's maximum length
      */
-    public function guessMaxLength($class, $property)
+    public function guessMaxLength(string $class, string $property)
     {
         return null;
     }
@@ -125,25 +185,30 @@ class BoomTypeGuesser implements FormTypeGuesserInterface
      *
      * @return Guess\ValueGuess|null A guess for the field's required pattern
      */
-    public function guessPattern($class, $property)
+    public function guessPattern(string $class, string $property)
     {
         return null;
     }
 
-    protected function readPhpDocAnnotations($class, $property)
+    /**
+     * @throws ReflectionException
+     */
+    protected function readPhpDocAnnotations(string $class, string $property): array
     {
-        $reflProperty = new \ReflectionProperty($class, $property);
+        $reflProperty = new ReflectionProperty($class, $property);
         $reader = new AnnotationReader();
         $ar = [];
 
-        if ($annotation = $reader->getPropertyAnnotation(
-            $reflProperty,
-            'W3com\\BoomBundle\\Annotation\\EntityColumnMeta'
-        )) {
+        if (
+            $annotation = $reader->getPropertyAnnotation(
+                $reflProperty,
+                'W3com\\BoomBundle\\Annotation\\EntityColumnMeta'
+            )
+        ) {
             $ar['choices'] = $annotation->choices;
             $ar['label'] = $annotation->description;
 
-            if ($annotation->choices != null){
+            if (null != $annotation->choices) {
                 $ar['type'] = 'choice';
             } else {
                 $ar['type'] = $annotation->type;
